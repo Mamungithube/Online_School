e762b5f26afe1cad5728c0dbe7f466243c8f8dbd
mamungithube <orroshidmdmamun50@gmail.com>
2024-10-26 21:46:54 +0600

    update

--- Course/models.py
+++ Course/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
 class Course(models.Model):
-    name = models.CharField(max_length = 20)
+    name = models.CharField(max_length=20)
     description = models.TextField()
     image = models.ImageField(upload_to="Course/images/")
+    month = models.IntegerField()
+    Course_fee = models.IntegerField()
 
     def __str__(self):
         return self.name
